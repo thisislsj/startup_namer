5f22f33fb4b8804ba5b3a321c864c8c10578220e
Lahiru Sandeepa <lahirusandeepa@outlook.com>
2019-09-19 15:37:55 +0530

    Can save favorite names

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,37 @@ class MyApp extends StatelessWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+  void _pushSaved() {
+    Navigator.of(context).push(
+       MaterialPageRoute<void>(  
+      builder: (BuildContext context) {
+        final Iterable<ListTile> tiles = _saved.map(
+          (WordPair pair) {
+            return ListTile(
+              title: Text(
+                pair.asPascalCase,
+                style: _biggerFont,
+              ),
+            );
+          },
+        );
+        final List<Widget> divided = ListTile
+          .divideTiles(
+            context: context,
+            tiles: tiles,
+          )
+          .toList();
+        return Scaffold(         
+          appBar: AppBar(
+            title: Text('Saved Suggestions'),
+          ),
+          body: ListView(children: divided),
+        );  
+      },
+    ),
+  );
+  }
+
 Widget _buildSuggestions() {
   return ListView.builder(
       padding: const EdgeInsets.all(16.0),
@@ -34,21 +65,39 @@ Widget _buildSuggestions() {
 }
 
 Widget _buildRow(WordPair pair) {
+  final bool alreadySaved = _saved.contains(pair);
   return ListTile(
     title: Text(
       pair.asPascalCase,
       style: _biggerFont,
     ),
+    trailing: Icon(   
+      alreadySaved ? Icons.favorite : Icons.favorite_border,
+      color: alreadySaved ? Colors.red : null,
+    ),    
+    onTap: () {      
+      setState(() {
+        if (alreadySaved) {
+          _saved.remove(pair);
+        } else { 
+          _saved.add(pair); 
+        } 
+      });
+    },      
   );
 }
 
   final _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();  
   final _biggerFont = const TextStyle(fontSize: 18.0);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
     appBar: AppBar(
       title: Text('Startup Name Generator'),
+      actions: <Widget>[   
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
     ),
     body: _buildSuggestions(),
   );
